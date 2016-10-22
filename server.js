aca260b80d72b2d76600128ddf00304866d5ffba
ssowemim <ssowemim@ualberta.ca>
2016-10-22 00:15:59 -0600

    rendering statis html pages successgit add -A

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'Femi Sowemimo',
+    likes: [
+      'Biking',
+      'Schooling',
+      'Studying'
+    ]
+  });
+});
+
+app.get('/about', (req,res) => {
+  res.send('About Page');
+});
+
+app.listen(3000, ()=> {
+  console.log('Server is up on port 3000');
+});
