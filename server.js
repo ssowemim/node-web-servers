c883fbb0f23d79dc02ed036ca1732575083ab24e
ssowemim <ssowemim@ualberta.ca>
2016-10-22 00:54:15 -0600

    calling it a night

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Femi Sowemimo',
-    likes: [
-      'Biking',
-      'Schooling',
-      'Studying'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to Hearthbreak.',
+    curentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req,res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
-app.listen(3000, ()=> {
+app.listen(4000, ()=> {
   console.log('Server is up on port 3000');
 });
