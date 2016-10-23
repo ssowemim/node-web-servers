cc6233f4386fabaf2e119eb639c053bfa327c795
ssowemim <ssowemim@ualberta.ca>
2016-10-22 20:12:40 -0600

    impleted partials into integration

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
@@ -11,7 +12,7 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to Hearthbreak.',
-    curentYear: new Date().getFullYear()
+    currentYear: new Date().getFullYear()
   });
 });
 
