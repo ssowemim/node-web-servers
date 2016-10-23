f09df2b872f9e38552420471d6d6af4be9dded7c
ssowemim <ssowemim@ualberta.ca>
2016-10-22 21:38:47 -0600

    adding heroku platform

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 5000;
 
 var app = express();
 
@@ -7,22 +10,33 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to Hearthbreak.',
-    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req,res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
-});  
+});
 
-app.listen(4000, ()=> {
-  console.log('Server is up on port 3000');
+app.listen(port, ()=> {
+  console.log(`Server is up on port ${port}`);
 });
